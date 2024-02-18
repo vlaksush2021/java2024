ee63d835effaab73ea029fa5639245ff2ceaf67a
vlaksush2021 <125420477+vlaksush2021@users.noreply.github.com>
2024-02-18 08:02:51 +0530

    Add files via upload

---
+++ FirstJava.java
@@ -0,0 +1,26 @@
+class FirstJava
+{
+	public static void main(String args[])
+	{
+	System.out.println("Welcome to Java");
+	}
+}
+
+/*
+1. java program is ready
+2. compile - translator => java ->binary(0 or 1)
+
+3. javac(compiler) => java into intermediate code (JVM)
+4. JVM - java virtual machine
+
+	1. load intermediate code
+	2. verify the code
+	3. change into binary code using java(interpreter) 
+5. java(interpreter)  => intermediate code  - binary code
+
+
+		javac			java
+FirstJava.java =======> FirstJava.class ======> FirstJava.exe 
+
+
+*/
